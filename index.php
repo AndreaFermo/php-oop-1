c234d21c6a3c29c16b61e8b5c227d26a16504117
Andrea Fermo <andreafermo.sd@gmail.com>
2023-05-04 15:04:14 +0200

    bonus 1

--- index.php
+++ index.php
@@ -4,22 +4,34 @@ class Movie {
     public $title;
     public $rate;
     public $originalLanguage;
+    public $genres;
 
-    public function __construct($title, $rate, $originalLanguage) {
+    public function __construct($title, $rate, $originalLanguage, $genres) {
         $this->title = $title;
         $this->rate = $rate;
         $this->originalLanguage = $originalLanguage;
+        $this->genres = $genres;
     }
 
     public function getFilmInfo() {
-        return '<br/><br/>Titolo: ' . $this->title . '<br/>Voto: ' . $this->rate . '<br/>Lingua originale: ' . $this->originalLanguage;
+        $genres = '';
+
+        foreach ($this->genres as $genre) {
+            $genres .= $genre . ' ' ;
+        }
+
+        return '<br/><br/>Titolo: ' . $this->title . '<br/>Voto: ' . $this->rate . '<br/>Lingua originale: ' . $this->originalLanguage . '<br/>Generi: ' . $genres;
     }
+
+
 
 }
 
-$starWars = new Movie('Guerre Stellari', 5, 'Inglese');
+$genresList = ['fantascienza', 'fantsy', 'avventura'];
+
+$starWars = new Movie('Guerre Stellari', 5, 'Inglese', $genresList);
 echo $starWars->getFilmInfo();
 
-$backToTheFuture = new Movie('Ritorno al futuro', 4, 'Inglese');
+$backToTheFuture = new Movie('Ritorno al futuro', 4, 'Inglese', $genresList);
 echo $backToTheFuture->getFilmInfo();
 
